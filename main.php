d5d69cdd90a5594d5aabb6463a5eab2ff19a498c
dan <d.c153boo@gmail.com>
2017-11-15 19:19:38 -0500

    Update main.php

--- main.php
+++ main.php
@@ -1,164 +1,225 @@
-<!DOCTYPE html>
-<html>
-<head> <meta http-equiv="refresh" content="1" ></head>
-    
-<?php 
-date_default_timezone_set("America/New_York");
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+/*
+   This sample code demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.
+   It requires the use of SoftwareSerial, and assumes that you have a
+   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
+*/
+static const int RXPin = 5, TXPin = 4;
+static const uint32_t GPSBaud = 9600;
 
+// The TinyGPS++ object
+TinyGPSPlus gps;
+String _lati = "", _longi = "";
+const char server[] = "smartcollar.000webhostapp.com"; 
 
+const char* MY_SSID = "P94KGH";
+const char* MY_PWD =  "PIKUSHASAFATBUTT101";
 
-$TimeStamp = "The current time is " . date("h:i:sa");
-//file_put_contents('dataDisplayer.html', $TimeStamp, FILE_APPEND);
-   if( isset($_POST["Latitude"]) ||  isset($_POST["Longitude"]) ) 
-   {
-   echo " The Humidity is: ". $_POST['Latitude']. "%<br />";
-   echo " The Temperature is: ". $_POST['Longitude']. " Celcius<br />";
-   }
-$var1 = isset($_POST['Latitude']) ? $_POST['Latitude'] : '';	  
-$var2 = isset($_POST['Longitude']) ? $_POST['Longitude'] : '';
-$WriteMyRequest = "<!DOCTYPE html>
-<html>
-  <head>
-      <link href='https://fonts.googleapis.com/css?family=Alef' rel='stylesheet'>
-    <style>
-       #map {
-        height: 400px;
-        width: 50%;
-       }
-        body {
-        width: auto;
-        margin: 20px;
-        font-family: 'Alef';font-size: 22px;
-        }
-        h2, h2+p {display: inline;}
-    </style>
-    <script type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js\"></script>
-    <script type=\"text/javascript\" src=\"maps.google.polygon.containsLatLng.js\"></script>
-    <script type=\"text/javascript\" src=\"http://maps.google.com/maps/api/js?sensor=false\"></script>
-    <script type=\"text/javascript\" src=\"http://jquery.offput.ca/js/jquery.timers.js\"></script>
-    <script type=\"text/javascript\" src=\"jquery-1.6.4-min.js\"></script>
-    <script type=\"text/javascript\" src=\"beautify.js\"></script>
-	<script type=\"text/javascript\" src=\"GeoJSON.js\"></script>
-     <meta http-equiv=\"refresh\" content=\"10\" >
-  </head>
-  <div class = \"content\">
-  <body>
-    <h2>Smart Collar </h2> <p>By D. Tsymbala, S. Budinoski, K. de Asis, A. Hedhli</p> <br/>".
-     $TimeStamp."<br/>".
-     "<p> Latitude : "       . $var1 . " </p>".
-     "<p> Longitude : " . $var2 . " </p> <br/>".
-     "<button type=\"button\">Activate Dog Whistle</button>
-     <input type=\"button\" value=\"Clear Safe Zone\" id=\"DeletePolygons\" >
-    <div id=\"map\"></div>
-    <script>
-    
-      $(document).ready(function(){
-    $('#DeletePolygons').click(function(){
-       localStorage.clear();
-    });
-  });
-    
-    
-      var map; var marker;
-      var safeZone = new google.maps.Polygon();
-      
-      function initMap() {
-        var uluru = {lat: " . 40.74 . " , lng: " . -74.10 . "};
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15,
-          center: uluru,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          mapTypeControl: false
-        });
-          marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-          map.data.setControls(['Polygon']);
-        map.data.setStyle({
-        editable: true,
-        draggable: true
-    });
+WiFiClient client;
 
-            bindDataLayerListeners(map.data);
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
 
-    //load saved data
-            loadPolygons(map);
+void setup()
+{
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+  Serial.print("Connecting to "+*MY_SSID);
+  WiFi.begin(MY_SSID, MY_PWD);
+  Serial.println("going into wl connect");
 
-      }
-      
-      
-      function bindDataLayerListeners(dataLayer) {
-    dataLayer.addListener('addfeature', savePolygon);
-    dataLayer.addListener('removefeature', savePolygon);
-    dataLayer.addListener('setgeometry', savePolygon);
-}
+  while (WiFi.status() != WL_CONNECTED) //not connected,  ...waiting to connect
+  {
+      delay(1000);
+      Serial.print(".");
+  }
 
-function loadPolygons(map) {
-    var data = JSON.parse(localStorage.getItem('geoData'));
-    map.data.forEach(function (f) {
-        map.data.remove(f);
-    });
-    map.data.addGeoJson(data);
-    
-     data.features.forEach(function(feat){
-   var geom=feat.geometry; 
-   var props=feat.properties;
-   if (geom.type === 'Polygon'){
-      for (var i=0; i < geom.coordinates.length; i++){
-          var polygon = { 
-               'type':'Polygon', 
-               'coordinates':[geom.coordinates[i]]
-          };
-      }
-      
+  Serial.println("wl connected");
+  Serial.println("");
+  Serial.println("Credentials accepted! Connected to wifi\n ");
+  Serial.println("");
   
+  Serial.println(F("FullExample.ino"));
+  Serial.println(F("An extensive example of many interesting TinyGPS++ features"));
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
+  Serial.println(F("Sats HDOP Latitude   Longitude   Fix  Date       Time     Date Alt    Course Speed Card  Distance Course Card  Chars Sentences Checksum"));
+  Serial.println(F("          (deg)      (deg)       Age                      Age  (m)    --- from GPS ----  ---- to London  ----  RX    RX        Fail"));
+  Serial.println(F("---------------------------------------------------------------------------------------------------------------------------------------"));
+  
+}
+
+void loop()
+{
+  static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
+
+  printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
+  printInt(gps.hdop.value(), gps.hdop.isValid(), 5);
+  printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
+  printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
+  printInt(gps.location.age(), gps.location.isValid(), 5);
+  printDateTime(gps.date, gps.time);
+  printFloat(gps.altitude.meters(), gps.altitude.isValid(), 7, 2);
+  printFloat(gps.course.deg(), gps.course.isValid(), 7, 2);
+  printFloat(gps.speed.kmph(), gps.speed.isValid(), 6, 2);
+  printStr(gps.course.isValid() ? TinyGPSPlus::cardinal(gps.course.value()) : "*** ", 6);
+
+  unsigned long distanceKmToLondon =
+    (unsigned long)TinyGPSPlus::distanceBetween(
+      gps.location.lat(),
+      gps.location.lng(),
+      LONDON_LAT, 
+      LONDON_LON) / 1000;
+  printInt(distanceKmToLondon, gps.location.isValid(), 9);
+
+  double courseToLondon =
+    TinyGPSPlus::courseTo(
+      gps.location.lat(),
+      gps.location.lng(),
+      LONDON_LAT, 
+      LONDON_LON);
+
+  printFloat(courseToLondon, gps.location.isValid(), 7, 2);
+
+  const char *cardinalToLondon = TinyGPSPlus::cardinal(courseToLondon);
+
+  printStr(gps.location.isValid() ? cardinalToLondon : "*** ", 6);
+
+  printInt(gps.charsProcessed(), true, 6);
+  printInt(gps.sentencesWithFix(), true, 10);
+  printInt(gps.failedChecksum(), true, 9);
+  Serial.println();
+
+
+
+    Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    WiFi.printDiag(Serial);
+
+    String data = "Latitude="
+          +                     (String) 40.7282 // (String) _lati 
+          +  "&Longitude="  +   (String) -74.0776; //(String) _longi
+
+     //change URL below if using your Sub-Domain
+     client.println("POST /main.php HTTP/1.1"); 
+     //change URL below if using your Domain
+     client.print("Host: smartcollar.000webhostapp.com\n");                 
+     client.println("User-Agent: ESP8266/1.0");
+     client.println("Connection: close"); 
+     client.println("Content-Type: application/x-www-form-urlencoded");
+     client.print("Content-Length: ");
+     client.print(data.length());
+     client.print("\n\n");
+     client.print(data);
+     client.stop();
+
+     Serial.println("\n");
+     Serial.println("My data string im POSTing looks like this: ");
+     Serial.println(data);
+     Serial.println("And it is this many bytes: ");
+     Serial.println(data.length());
+     smartDelay(2000);
     }
-    
-    
-    var newPoly = new GeoJSON(polygon);
-    safeZone = newPoly;
 
-        google.maps.event.addListener(map, 'idle', function() {
-    if(google.maps.geometry.poly.containsLocation(marker.getPosition(),safeZone)){
-          alert(\"true\");
-     }else{ alert(\"false\"); 
-         
-       //  $.ajax({ url: '/call.php',
-       //  type: 'post'
-       //     });
-       
-           $.ajax({ url: '/send.php',
-           type: 'post'
-              });
-         
-     }
-        });
-    
- });
-        
-        
+
+
+
+
+  
+  
+  smartDelay(1000);
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+    Serial.println(F("No GPS data received: check wiring"));
+
+
 }
 
-
-
-function savePolygon() {
-    map.data.toGeoJson(function (json) {
-        localStorage.setItem('geoData', JSON.stringify(json));
-    });
+// This custom version of delay() ensures that the gps object
+// is being "fed".
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
 }
 
+static void printFloat(float val, bool valid, int len, int prec)
+{
+  if (!valid)
+  {
+    while (len-- > 1)
+      Serial.print('*');
+    Serial.print(' ');
+  }
+  else
+  {
+    Serial.print(val, prec);
+    _lati = (String) gps.location.lat();
+    _longi = (String) gps.location.lng();
+    int vi = abs((int)val);
+    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+    for (int i=flen; i<len; ++i)
+      Serial.print(' ');
+  }
+  smartDelay(0);
+}
 
-      
-    </script>
-    <script async defer
-    src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyBdZlGc7e2SPKvzRuPaoASDAzYHofsdBRk&libraries=drawing&callback=initMap\">
-    </script>
-  </body>
-  </div>
-</html>" ;
-echo $WriteMyRequest;
-file_put_contents('index.html', $WriteMyRequest);
-?>
+static void printInt(unsigned long val, bool valid, int len)
+{
+  char sz[32] = "*****************";
+  if (valid)
+    sprintf(sz, "%ld", val);
+  sz[len] = 0;
+  for (int i=strlen(sz); i<len; ++i)
+    sz[i] = ' ';
+  if (len > 0) 
+    sz[len-1] = ' ';
+  Serial.print(sz);
+  smartDelay(0);
+}
 
-</html>
+static void printDateTime(TinyGPSDate &d, TinyGPSTime &t)
+{
+  if (!d.isValid())
+  {
+    Serial.print(F("********** "));
+  }
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d/%02d/%02d ", d.month(), d.day(), d.year());
+    Serial.print(sz);
+  }
+  
+  if (!t.isValid())
+  {
+    Serial.print(F("******** "));
+  }
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
+    Serial.print(sz);
+  }
+
+  printInt(d.age(), d.isValid(), 5);
+  smartDelay(0);
+}
+
+static void printStr(const char *str, int len)
+{
+  int slen = strlen(str);
+  for (int i=0; i<len; ++i)
+    Serial.print(i<slen ? str[i] : ' ');
+  smartDelay(0);
+}
